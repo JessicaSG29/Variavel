d6223d836965f72c6ced4c10b55faba0721023f0
JessicaSG29 <130482555+JessicaSG29@users.noreply.github.com>
2023-06-14 19:28:02 -0300

    Add files via upload

---
+++ Variáveis/arquivo.js
@@ -0,0 +1,13 @@
+var pagina= "A pagina foi carregada com sucesso "
+alert(pagina)
+
+var minhaidade=30;
+var idadenamorado=33;
+
+document.write("A soma das nossas idades são :" +(minhaidade+idadenamorado));
+
+document.write(" Idade do meu namorado dividida por dois é : " +(idadenamorado/2));
+
+document.write ("Minha idade multiplicada por 6 é : " +(minhaidade*8));
+
+document.write (" Nossas idades são iguais ? " + (minhaidade==idadenamorado));
